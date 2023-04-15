f56c5bc8232e8fdcdbc5c3ab87036efdb87c4562
BYRON LOARTE <by_tosh20@hotmail.com>
2023-04-15 17:48:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+ubicacion = input("Ingrese su ubicación: ")
+edad = int(input("Ingrese su edad: "))
+
+print(f"\n\n Bienvenido {nombre} - {apellido}, su ubicación es {ubicacion} y su edad es de {edad} años")
